5d7e363ea23bca85710dc1fffb3ce03c3b2229bf
didighgh0451-wq <didighgh0451@gmail.com>
2026-02-20 13:32:14 +0900

    Update projectData.ts

--- src/app/components/projectData.ts
+++ src/app/components/projectData.ts
@@ -244,7 +244,7 @@ export const projects: Project[] = [
     year: '2025',
     client: 'Freelance',
     image:
-      'https://images.unsplash.com/photo-1758553026412-bc1da0ebd366?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHx2aWRlbyUyMGVkaXRpbmclMjBjaW5lbWF0aWMlMjBwcm9kdWN0aW9ufGVufDF8fHx8MTc3MDcyODM3NXww&ixlib=rb-4.1.0&q=80&w=1080&utm_source=figma&utm_medium=referral',
+      '/4.png',
     videoUrl: 'https://youtu.be/YBdPs9yOyZY',
     aspectRatio: '16:9',
     description:
